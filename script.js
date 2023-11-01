1084c1cfec88faa5483d4ca1e6e4cfb20c8c55b7
AffanShaikh100 <133389464+AffanShaikh100@users.noreply.github.com>
2023-11-01 16:50:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+let button = document.getElementById("btn");
+button.addEventListener("click", getdisplaydata);
+
+async function getdisplaydata(){
+	let text = document.getElementById("text").value;
+	let delay =  document.getElementById("delay").value
+    let div = document.getElementById("output");
+	let promisedata = new Promise ((resoleve,reject)=>{
+		setTimeout(()=>{
+			resoleve(text);
+			console.log("wfhbvhvbhb");
+		},delay)
+	})
+
+	promisedata.then((value)=>{
+		div.textContent = value
+	}).catch((error)=>{
+		div.innerHtml =error
+	})
+}
